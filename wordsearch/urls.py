eca2891787ee0ac32a37cd1d6ebe98fa53a6f71d
Mark Riley Bomberry <higgsofwolfram74@gmail.com>
2021-08-29 16:10:44 -0500

    Starting to implement json responses

--- wordsearch/urls.py
+++ wordsearch/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         path('', views.home, name='wordsearch-home'),
+        path('q/<str:words>/', views.jquery, name='wordsearch-json')
 ]
--- wordsearch/views.py
+++ wordsearch/views.py
@@ -1,5 +1,19 @@
+from typing import Optional
 from django.shortcuts import render
+from django.http import JsonResponse
+from . import serializer
 
+#submit words with GET, return wordsearch
 # Create your views here.
 def home(request):
-    return render(request, "wordsearch/base.html")
+    return render(request, "wordsearch/home.html")
+
+def jquery(request, words="") -> Optional[JsonResponse]:
+    if not words:
+        return None
+    
+    word_dict = serializer.serializer(words)
+
+    return JsonResponse(word_dict)
+        
+
