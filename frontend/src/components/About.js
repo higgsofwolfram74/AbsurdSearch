a103dcc38e846f5336db5c8b164ce2f310d1e868
Mark Riley Bomberry <higgsofwolfram74@gmail.com>
2021-12-24 03:36:52 -0600

    making sure everything is updated

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 export default function About() {
-  return <p>Test</p>;
+  return (
+    <div>
+      <Typography variant="h5" component="div">
+        Hello! This is a fun website I made to practice exposing API's to <br />{" "}
+        my other projects for my friends to use. Feel free to generate <br />
+        wordsearches and use the solver on them as you please. <br /> If you
+        have any problems, please go to{" "}
+        <Link href="https://www.github.com/higgsofwolfram74">
+          my github
+        </Link>{" "}
+        and create <br />
+        an issue there.
+      </Typography>
+    </div>
+  );
 }
--- frontend/src/components/Wordsearchresult.js
+++ frontend/src/components/Wordsearchresult.js
@@ -1,6 +1,27 @@
 import React from "react";
 import { useEffect } from "react";
 
+function exportToJson(objectData) {
+  let filename = "Wordsearch.json";
+  let contentType = "application/json;charset=utf-8;";
+
+  var a = document.createElement("a");
+  a.download = filename;
+  a.href =
+    "data:" +
+    contentType +
+    "," +
+    encodeURIComponent(JSON.stringify(objectData));
+  a.target = "_blank";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
 export default function Wordsearchresult(props) {
+  /* useEffect(() => {
+    exportToJson(props);
+  }); */
+
   return <div>thanks</div>;
 }
